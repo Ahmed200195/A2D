76b3f3db69e4f4612d061cd9c3493aad5e8d6bbc
Ahmed Delwan <74209083+Ahmed200195@users.noreply.github.com>
2022-02-06 17:32:00 +0200

    1

---
+++ Hire Me/Classes/Access_DataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Hire_Me.Classes
+{
+    public class Access_DataBase
+    {
+        OracleConnection connection;
+        string name_con;
+        public Access_DataBase()
+        {
+            name_con = ConfigurationManager.ConnectionStrings["tcc_con"].ConnectionString;
+            connection = new OracleConnection(name_con);
+        }
+    }
+}
