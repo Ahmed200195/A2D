31090821226d699ed2c79c0e676710914fda08bf
Ahmed Delwan <74209083+Ahmed200195@users.noreply.github.com>
2022-02-08 10:49:06 +0200

    Add

--- Hire Me/Classes/Access_DataBase.cs
+++ Hire Me/Classes/Access_DataBase.cs
@@ -3,18 +3,56 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Data;
 using Oracle.ManagedDataAccess.Client;
 
 namespace Hire_Me.Classes
 {
+    enum KeyWord
+    {
+        Select, DML
+    }
     public class Access_DataBase
     {
         OracleConnection connection;
-        string name_con;
+        DataTable dataTable;
+        OracleCommand oracleCommand;
+        OracleDataAdapter dataAdapter;
+
+        //Create Connection with DataBase
         public Access_DataBase()
         {
-            name_con = ConfigurationManager.ConnectionStrings["tcc_con"].ConnectionString;
-            connection = new OracleConnection(name_con);
+            connection = new OracleConnection();
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["tcc_con"].ConnectionString;
+            oracleCommand = connection.CreateCommand();
+        }
+        //Open and Close Connection with DataBase
+        public void ConnectToggel()
+        {
+            if(connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            else
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+        //Select Data From DataBase
+        public DataTable ProcessData(string Query)
+        {
+            oracleCommand.CommandText = Query;
+            dataAdapter = new OracleDataAdapter(oracleCommand);
+            dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        public void Ex_DML(string Query)
+        {
+            oracleCommand.CommandText = Query;
+            oracleCommand.ExecuteNonQuery();
         }
     }
 }
--- Hire Me/Home.aspx.cs
+++ Hire Me/Home.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Hire_Me.Classes;
+using System.Data;
 
 namespace Hire_Me
 {
     public partial class Home : System.Web.UI.Page
     {
+        Access_DataBase access;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            access = new Access_DataBase();
+            access.ConnectToggel();
+            access.Ex_DML("insert int nemp(empno) values(1000)");
+            GridView1.DataSource = access.ProcessData("select * from nemp");
+            GridView1.DataBind();
+            access.ConnectToggel();
         }
     }
 }
