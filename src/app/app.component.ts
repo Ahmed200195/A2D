91016bbfe369b767195176765c9af68ab93cf953
Ahmed Delwan <74209083+Ahmed200195@users.noreply.github.com>
2024-08-11 08:17:33 +0300

    uu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, NgFor, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  colors = ['Red', 'Blue', 'Green'];
+
   title = 'MyAngularProject';
 
   name="Ahmed Delwan";
+  age = 23;
+
+  isAuth:boolean = false;
+  stateLog = "Login";
+
+  toggleLogin(){
+    this.isAuth = !this.isAuth;
+    this.stateLog = this.isAuth ? "Logout" : "Login";
+  }
+
+  MyFunction(){
+    alert("Hello !!!");
+  }
+
+  OnChange(){
+    alert("Ok Changed!");
+  }
 }
